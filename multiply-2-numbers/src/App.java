011b62e79a0d8ff268554cc4b330d29103213c6e
Bruno Santos <bms.santos@outlook.pt>
2025-09-26 17:40:53 +0100

    multiply-2-numbers

---
+++ multiply-2-numbers/src/App.java
@@ -0,0 +1,8 @@
+public class App {
+    public static void main(String[] args) {
+        int num1 = 2;
+        int num2 = 6;
+
+        System.out.println(num1 + " x " + num2 + " = " + (num1*num2));
+    }
+}
