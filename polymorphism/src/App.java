760dd77d80ce856d2a2788875abaa49a6a6ce9ba
morangoo <bms.santos@outlook.pt>
2025-09-27 17:31:35 +0100

    polymorphism

---
+++ polymorphism/src/App.java
@@ -0,0 +1,9 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        Animal myPig = new Pig();
+        Animal myDog = new Dog();
+
+        myPig.animalSound();
+        myDog.animalSound();
+    }
+}
---
+++ polymorphism/src/Dog.java
@@ -0,0 +1,7 @@
+public class Dog extends Animal {
+    @Override
+    public void animalSound() {
+        System.out.println("Woof woof!");
+    }
+
+}
