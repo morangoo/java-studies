7fcd261c4fe0361ea04d678aa5ba10e73ea72784
morangoo <bms.santos@outlook.pt>
2025-10-11 13:28:34 +0100

    seed: params

--- blip/seed/src/main/java/com/morangoo/github/seed/HelloWorldController.java
+++ blip/seed/src/main/java/com/morangoo/github/seed/HelloWorldController.java
@@ -2,12 +2,13 @@ package com.morangoo.github.seed;
 
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
 public class HelloWorldController {
 
     @GetMapping("/hello")
-    public String hello() {
-        return "Hello, World!";
+    public String hello(@RequestParam(name = "name", defaultValue="World") String name) {
+        return "Hello, " + name + "!";
     }
 }
